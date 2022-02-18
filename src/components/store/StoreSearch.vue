4d44f5f4b383183e33469ab861b4ed5f3389babd
unknown <didriko@live.no>
2022-02-18 16:39:28 +0100

    ..

---
+++ src/components/store/StoreSearch.vue
@@ -0,0 +1,28 @@
+<!--DIDRIKS KODER -->
+
+<template>
+    <input v-model="searchInput" type="text">
+    <input @click="searchFunction" type="button" value="search">
+</template>
+
+<script>
+import {ref} from 'vue'
+import storeServices from '../../services/storeServices'
+
+export default {
+
+    setup() {
+       
+       const searchInput = ref(""); 
+
+        const searchFunction = () => {
+            console.log("Du søkte etter: " + searchInput.value)
+            storeServices.filterItems(searchInput.value)
+        }
+
+        return {searchInput, searchFunction }
+    },
+
+}
+
+</script>*/
